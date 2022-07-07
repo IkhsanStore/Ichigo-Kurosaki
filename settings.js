8d6c9e15fd47048dd5e255ec6d9debba364622c4
IKHSAN STORE <91927659+IkhsanStore@users.noreply.github.com>
2022-07-07 08:05:05 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,18 +9,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285767669919','6285767669919','0']
-global.ownerName = 'SANS JB'
-global.packname = 'SANS JB'
-global.author = 'BOT BY SANS JB'
+global.ownerNumber = ['6285767669919','6285767669919','']
+global.ownerName = 'SANS STORE'
+global.packname = 'SANS STORE'
+global.author = 'BOT BY SANS STORE'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'session.json'
 
 //Message Nya Ubah Disini
 global.mess = {
-admin: 'SANS JB',
-botAdmin: 'SANS JB',
-botOwner: 'SANS JB',
+admin: 'SANS STORE',
+botAdmin: 'SANS STORE',
+botOwner: 'SANS STORE',
 group: 'Only Group!',
 private: 'Only Private Chat',
 wait: 'Loading...',
